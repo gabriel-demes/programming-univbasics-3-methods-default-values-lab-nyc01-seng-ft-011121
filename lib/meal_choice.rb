84285aeb166b2b3430fd98d127e169e4afbe8414
Gabriel Demes <gabrieldemes@nyu.edu>
2020-11-23 23:57:53 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,5 +5,5 @@ def meal_choice(protein = "tofu", veg1, veg2)
   puts "What a nutritous meal!"
   message = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts message
-  return message
+  message
 end
